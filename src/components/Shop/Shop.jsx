e0a7ec2b9eaf53cc92aef7ddbe090e6a3630126f
sagorforu <freelancersagor18@gmail.com>
2023-03-29 03:29:31 +0600

    retrive product and save into local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,8 +15,24 @@ const Shop = () => {
 
     useEffect( () =>{
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    },[])
+        const savedCart = [];
+        // step 1: get id 
+        for(const id in storedCart){
+            // console.log(id)
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step 3: get the quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the addedProduct to the saved cart
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct)
+        }
+        // step 5: set the cart
+        setCart(savedCart)
+    },[products])
 
     const handleAddToCart = (product) => {
         // cart.push(product); 
